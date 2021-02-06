a25f390c2dc835a586dde74aecff13cf936b52e8
Edder Serna <edderserna22@gmail.com>
2021-02-05 22:43:25 -0600

    estilos y tabla

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function createNode(type, child) {
 }
 
 
-document.body.appendChild(createNode('h1', 'To Do App'));
+document.body.appendChild(createNode('h1', 'TO DO APP'));
 document.body.appendChild(document.createElement('form'));
 
 var field = document.createElement('INPUT');
@@ -21,7 +21,7 @@ field.setAttribute('placeholder', 'New Task...');
 
 
 document.getElementsByTagName('form')[0].appendChild(field);
-document.getElementsByTagName('form')[0].appendChild(createNode('BUTTON', 'Add'));
+document.getElementsByTagName('form')[0].appendChild(createNode('BUTTON', 'Add →'));
 
 
 document.body.appendChild(document.createElement('ul')).appendChild(createNode('li', 'Introduzca texto'));
