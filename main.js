a272980efeccd83e2d5f3ebdeb535999ea7a5b27
Edder Serna <edderserna22@gmail.com>
2021-02-05 21:20:03 -0600

    actualizacion uno

--- main.js
+++ main.js
@@ -12,14 +12,16 @@ function createNode(type, child) {
 }
 
 
-
 document.body.appendChild(createNode('h1', 'To Do App'));
-document.body.appendChild(document.createElement("form"));
+document.body.appendChild(document.createElement('form'));
 
-var field = document.createElement("INPUT");
-field.setAttribute("type", "text");
-field.setAttribute("placeholder", "New Task...");
+var field = document.createElement('INPUT');
+field.setAttribute('type', 'text');
+field.setAttribute('placeholder', 'New Task...');
 
 
 document.getElementsByTagName('form')[0].appendChild(field);
 document.getElementsByTagName('form')[0].appendChild(createNode('BUTTON', 'Add'));
+
+
+document.body.appendChild(document.createElement('ul')).appendChild(createNode('li', 'Introduzca texto'));
