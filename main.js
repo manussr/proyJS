5c41aa4837187cd1e47876555dd3f1740a86332c
Edder Serna <edderserna22@gmail.com>
2021-02-05 19:23:51 -0600

    estructura inicial

---
+++ main.js
@@ -0,0 +1,25 @@
+function createNode(type, child) {
+    var node = document.createElement(type);
+
+    if (typeof child === "string") {
+        var text = document.createTextNode(child);
+        node.appendChild(text);
+    } else {
+        node.appendChild(child);
+    }
+
+    return node;
+}
+
+
+
+document.body.appendChild(createNode('h1', 'To Do App'));
+document.body.appendChild(document.createElement("form"));
+
+var field = document.createElement("INPUT");
+field.setAttribute("type", "text");
+field.setAttribute("placeholder", "New Task...");
+
+
+document.getElementsByTagName('form')[0].appendChild(field);
+document.getElementsByTagName('form')[0].appendChild(createNode('BUTTON', 'Add'));
